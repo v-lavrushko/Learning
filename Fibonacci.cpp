b40caeb31f2bf76c7ee306e35e04a9c9e79dd5a8
v-lavrushko <Landadang@Gmail.com>
2017-07-13 13:14:43 +0300

    Update Fibonacci.cpp

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -2,33 +2,18 @@
 #include <iostream>
 using namespace std;
 
-int looking(long a, long b)
-{
-	do {
-		a--;
-		if (b%a == 0)
-			continue;
-	} while (a>1);
-}
-bool looking2(long a, long b)
-{
-	do {
-		a--;
-		if (b%a == 0) {
-			return false;
-			continue; }
-		else
-			return true;
-	} while (a>1)
-			
-}
 int main()
 {
-	long a(600851475143), b = a;
-	a = looking(a, b);
-	looking2 (a, a);
-	if (looking2 == false)
-		a = looking(a, b);
-	else
-		cout << "The answer is " << a << endl;
+	long a(1), b(0), c(0), d(0);
+	do {
+		cout << a << " ";
+		c = a;
+		a = a + b;
+		b = c;
+		if (a % 2 == 0)
+			d = a + d;
+		else
+			d = d;
+	} while (a < 4000000);
+	cout << endl << "The ansver is " << d << endl;
 }
